1cfab7240780795273c2f8e6586c3bd4821699f8
agranato-c <agranato@corradino.com>
2025-06-26 09:53:53 -0400

    git commit -m “initial commit”

--- src/slices/ScrollText/index.tsx
+++ src/slices/ScrollText/index.tsx
@@ -87,38 +87,31 @@ const ScrollText: FC<ScrollTextProps> = ({ slice }) => {
       ref={componentRef}
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="flex h-screen items-center justify-center bg-neutral-950 relative"
+      className="relative flex h-screen items-center justify-center bg-neutral-950"
     >
-      <div className="glow-background absolute inset-0 z-0 h-full w-full opacity-0">
-
-      </div>
-      <div className="absolute inset-0 bg-[url('/noisetexture.jpg')] opacity-30 mix-blend-multiply">
-
-      </div>
+      <div className="glow-background absolute inset-0 z-0 h-full w-full opacity-0"> </div>
+      <div className="absolute inset-0 bg-[url('/noisetexture.jpg')] opacity-30 mix-blend-multiply"> </div>
 
       <div ref={contentRef}>
         <div className="mb-2 text-center text-sm tracking-wider text-neutral-200 uppercase md:mb-8 md:text-base">
-          { slice.primary.eyebrow }
+          {slice.primary.eyebrow}
         </div>
 
         {/* Paragraph */}
         <div ref={textRef} className="text-center">
           <p className="font-display flex flex-wrap justify-center text-5xl leading-tight text-balance uppercase md:text-7xl">
             {words.map((word, index) => (
-              <span key={`${word}-${index}`}>
-                className="inline"
-                  {word.split("").map((char, charIndex) => (
-                    <span key={`${char}-${charIndex}`}
-                      className="inline">
-                      {char}
-                    </span>
-                  ))}
-                  {index < words.length - 1 ? (
-                    <span className="inline">
-                      & nbsp;
-                    </span>
-                  ) : null}
-
+              <span key={`${word}-${index}`} className="inline">
+                {word.split("").map((char, charIndex) => (
+                  <span key={`${char}-${charIndex}`} className="inline">
+                    {char}
+                  </span>
+                ))}
+                {index < words.length - 1 ? (
+                  <span className="inline">
+                    & nbsp;
+                  </span>
+                ) : null}
               </span>
             ))}
           </p>
@@ -126,7 +119,6 @@ const ScrollText: FC<ScrollTextProps> = ({ slice }) => {
       </div>
     </Bounded>
   );
-    
 };
 
 export default ScrollText;
